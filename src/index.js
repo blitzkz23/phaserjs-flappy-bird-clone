49bf7ae67eea52a0b20d25b6144da799758ba5e2
Naufal Aldy Pradana <blitzaldykazu@gmail.com>
2021-04-22 20:59:56 +0700

    Add sprite, flap, and restart function

--- src/index.js
+++ src/index.js
@@ -8,33 +8,53 @@ const config = {
   height: 600,
   physics: {
     // Arcade ohysics plugin, manages physics simulation
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      gravity: { y:400 },
+      debug: true
+    }
   },
   scene: {
     preload,
     create,
+    update
   }
 }
 
-// Loading assets, such as images, music, animations
 function preload() {
-  // 'this' context - scene
-  // contains functions and properties we can use
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-  
 }
+
+const VELOCITY = 200;
 
 let bird = null;
+const initialBirdPosition = {x: config.width * 0.1, y: config.height / 2}
+let flapVelocity = 300;
+
 
 function create() {
-  // (x, y, key of the image)
-  // (400, 300 will center the background image, because (0,0) is the edge)
-  // this.add.image(config.width / 2, config.height / 2, 'sky');
-  /** but lets use the origin instead */
   this.add.image(0, 0, 'sky').setOrigin(0);
-  // middle of the height and 1/10 of weight location
-  bird = this.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
-  //debugger
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y,'bird').setOrigin(0);
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
 }
+
+function update(time, delta) {
+  if(bird.y <= 0 - bird.height || bird.y > config.height) {
+    restartBirdPosition();
+  }
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;  
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity
+}
+
 new Phaser.Game(config);
