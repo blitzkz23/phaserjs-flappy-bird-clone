4dd55b73291992456d0e7d95f18ffb607e61e2d9
Naufal Aldy Pradana <blitzaldykazu@gmail.com>
2021-04-23 19:19:06 +0700

    Refactored Code

--- src/index.js
+++ src/index.js
@@ -13,109 +13,15 @@ const SHARED_CONFIG = {
 }
 
 const config = {
-  // WebGL (Web graphics library) JS Api for rendering 2D and 3D graphics
   type: Phaser.AUTO,
   ...SHARED_CONFIG,
   physics: {
-    // Arcade physics plugin, manages physics simulation
     default: 'arcade',
     arcade: {
       debug: true,
     }
   },
   scene: [new PlayScene(SHARED_CONFIG)]
-}
-
-const VELOCITY = 200;
-const PIPES_TO_RENDER = 4;
-
-let bird = null;
-let pipes = null;
-
-let pipeHorizontalDistance = 0;
-
-const pipeHorizontalDistanceRange = [300, 450];
-const pipeVerticalDistanceRange = [150, 250];
-
-let flapVelocity = 300;
-const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
-
-
-function preload() {
-  
-  this.load.image('pipe', 'assets/pipe.png');
-}
-
-function create() {
-   
-
-  pipes = this.physics.add.group();
-
-  for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0,1);
-    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0,0);
-
-    placePipe(upperPipe, lowerPipe);
-  }
-  
-  pipes.setVelocityX(-200);
-
-  this.input.on('pointerdown', flap);
-  this.input.keyboard.on('keydown_SPACE', flap);
-}
-  
-function update(time, delta) {
-  if(bird.y <= 0 - bird.height || bird.y > config.height) {
-    restartBirdPosition();
-  }
-
-  recyclePipes();
-}
-
-function placePipe(uPipe, lPipe) {
-  
-  const rightMostX = getRightMostPipe();
-  const pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-  const pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
-  const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange);
-
-  uPipe.x = rightMostX + pipeHorizontalDistance;
-  uPipe.y = pipeVerticalDistance;
-
-  lPipe.x = uPipe.x;
-  lPipe.y = uPipe.y + pipeVerticalDistance;
-}
-
-function recyclePipes() {
-  const tempPipes = [];
-  pipes.getChildren().forEach(pipe => {
-    if(pipe.getBounds().right <= 0) {
-      tempPipes.push(pipe);
-      if (tempPipes.length == 2) {
-        placePipe(...tempPipes);
-      }
-    }
-  });
-}
-
-function getRightMostPipe() {
-  let rightMostX = 0;
-
-  pipes.getChildren().forEach(function(pipe) {
-    rightMostX = Math.max(pipe.x, rightMostX);
-  });
-
-  return rightMostX;
-}
-
-function restartBirdPosition() {
-  bird.x = initialBirdPosition.x;
-  bird.y = initialBirdPosition.y;
-  bird.body.velocity.y = 0;  
-}
-
-function flap() {
-  bird.body.velocity.y = -flapVelocity
 }
 
 new Phaser.Game(config);
